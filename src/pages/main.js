70501f6934fee32a7207b32ea68d6b3747e0d7f6
kateiscoding <kiaksh717@gmail.com>
2023-12-01 16:28:33 -0800

    feat: set default layout

---
+++ src/pages/main.js
@@ -0,0 +1,12 @@
+import MainPage from '../components/pages/main/MainPage';
+import DefaultLayout from '../components/layout/DefaultLayout';
+
+function Main() {
+	return (
+		<DefaultLayout>
+			<MainPage />
+		</DefaultLayout>
+	);
+}
+
+export default Main;
