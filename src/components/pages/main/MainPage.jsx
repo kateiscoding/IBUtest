d0a1c60b166565ab521d4d84bf2357071ec505c0
kateiscoding <kiaksh717@gmail.com>
2023-12-04 17:06:32 -0800

    feat: modified flag box css

--- src/components/pages/main/MainPage.jsx
+++ src/components/pages/main/MainPage.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
 import EarthImage from '../../../assets/earth-image-2.png';
 import InbodyMan from '../../../assets/inbodyman.png';
+import FlagContainer from '../../Flags';
 
 function MainPage() {
 	return (
@@ -9,13 +10,15 @@ function MainPage() {
 			<Counter>
 				<div>
 					<b>TOTAL</b>
+
+					<p>110,000,378</p>
 					<div>
 						<span>LIVE</span>
 					</div>
-					<p>110,000,378</p>
 				</div>
 			</Counter>
-			<FlagContainer>
+			<FlagContainer />
+			{/* <FlagContainer>
 				<Flagbox>
 					<FlagContent>
 						<p>China</p>
@@ -64,7 +67,7 @@ function MainPage() {
 						<img src={InbodyMan} alt='InbodyMan' />
 					</Gps>
 				</Flagbox>
-			</FlagContainer>
+			</FlagContainer> */}
 			<EarthContainer>
 				<EarthImageComponent src={EarthImage} alt='earth-image' />
 			</EarthContainer>
@@ -85,7 +88,7 @@ const Counter = styled.div`
 	height: 60rem;
 	width: 100%;
 	display: flex;
-	margin-top: 10rem;
+	margin-top: 14rem;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
@@ -101,6 +104,7 @@ const Counter = styled.div`
 			line-height: normal;
 			letter-spacing: 0.66rem;
 			flex-direction: column;
+			align-items: flex-start;
 		}
 		> div {
 			background: #971b2f;
@@ -129,7 +133,7 @@ const Counter = styled.div`
 			color: #fff;
 
 			text-align: center;
-			font-size: 11.25rem;
+			font-size: 16rem;
 			font-style: normal;
 			font-weight: 600;
 			line-height: normal;
@@ -137,83 +141,83 @@ const Counter = styled.div`
 	}
 `;
 
-const FlagContainer = styled.div`
-	color: white;
-	display: flex;
-	gap: 16rem;
-	${'' /* height: 30rem; */}
-	flex-direction: row;
-	justify-content: center;
-	align-items: flex-end;
-	width: 100%;
+// const FlagContainer = styled.div`
+// 	color: white;
+// 	display: flex;
+// 	gap: 60rem;
+// 	${'' /* height: 30rem; */}
+// 	flex-direction: row;
+// 	justify-content: center;
+// 	align-items: flex-end;
+// 	width: 100%;
 
-	position: absolute;
-	top: 40rem;
-	z-index: 2;
-`;
+// 	position: absolute;
+// 	top: 45rem;
+// 	z-index: 2;
+// `;
 
-const RedLine = styled.div`
-	display: flex;
+// const RedLine = styled.div`
+// 	display: flex;
 
-	flex-direction: column;
-	align-items: center;
+// 	flex-direction: column;
+// 	align-items: center;
 
-	> div {
-		width: 1px;
-		height: 15rem;
-		border-left: 4px dashed #971b2f;
+// 	> div {
+// 		width: 1px;
+// 		height: 15rem;
+// 		border-left: 4px dashed #971b2f;
 
-		flex-shrink: 0;
-		display: flex;
-	}
-`;
+// 		flex-shrink: 0;
+// 		display: flex;
+// 	}
+// `;
 
-const Flagbox = styled.div`
-	display: flex;
-	flex-direction: column;
-`;
+// const Flagbox = styled.div`
+// 	display: flex;
+// 	flex-direction: column;
+// `;
 
-const FlagContent = styled.div`
-	color: white;
-	display: flex;
-	width: 24rem;
-	height: auto;
-	padding: 2rem 3.5rem;
-	flex-direction: column;
-	align-items: center;
-	${'' /* gap: 2.5rem; */}
-	display: flex;
-	border-radius: 0.75rem;
-	background: rgba(255, 255, 255, 0.15);
+// const FlagContent = styled.div`
+// 	color: white;
+// 	display: flex;
+// 	width: 24rem;
+// 	height: auto;
+// 	padding: 2rem 3.5rem;
+// 	flex-direction: column;
+// 	align-items: center;
+// 	${'' /* gap: 2.5rem; */}
+// 	display: flex;
+// 	border-radius: 0.75rem;
+// 	background: rgba(255, 255, 255, 0.15);
 
-	box-shadow: 0px 4px 30px 0px rgba(0, 0, 0, 0.3);
-	backdrop-filter: blur(12px);
-	gap: 2.5rem;
-	> p {
-		color: #fff;
-		text-align: center;
-		font-size: 4rem;
-		font-style: normal;
-		font-weight: 700;
-		line-height: normal;
-	}
+// 	box-shadow: 0px 4px 30px 0px rgba(0, 0, 0, 0.3);
+// 	backdrop-filter: blur(12px);
+// 	gap: 2.5rem;
+// 	> p {
+// 		color: #fff;
+// 		text-align: center;
+// 		font-size: 4rem;
+// 		font-style: normal;
+// 		font-weight: 700;
+// 		line-height: normal;
+// 	}
 
-	> div {
-		display: flex;
-		padding: 0.5rem 0.25rem;
-		justify-content: center;
-		align-items: center;
-		gap: 0.5rem;
-		align-self: stretch;
-		border-radius: 0.25rem;
-		background: rgba(255, 255, 255, 0.1);
-	}
-`;
+// 	> div {
+// 		display: flex;
+// 		padding: 0.5rem 0.25rem;
+// 		justify-content: center;
+// 		align-items: center;
+// 		gap: 0.5rem;
+// 		align-self: stretch;
+// 		border-radius: 0.25rem;
+// 		background: rgba(255, 255, 255, 0.1);
+// 	}
+// `;
 
-const Gps = styled.div`
-	display: flex;
-	justify-content: center;
-`;
+// const Gps = styled.div`
+// 	display: flex;
+// 	justify-content: center;
+// `;
 
 const EarthContainer = styled.div`
 	width: 100%;
