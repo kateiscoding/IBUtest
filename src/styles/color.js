485edc407fd56b41522384e994308b03626d9d45
kateiscoding <kiaksh717@gmail.com>
2023-12-04 17:06:11 -0800

    style: added style constant

---
+++ src/styles/color.js
@@ -0,0 +1,85 @@
+export default class Colors {
+	static white = '#FFFFFF';
+
+	static black = '#000000';
+
+	/* white */
+
+	static white100 = {
+		color: '#FFFFFF',
+		opacity: '0.1',
+	};
+
+	static white200 = {
+		color: '#FFFFFF',
+		opacity: '0.2',
+	};
+
+	static white600 = {
+		color: '#FFFFFF',
+		opacity: '0.6',
+	};
+
+	/* red */
+
+	static brandci = '#971B2F';
+
+	static primary = '#CC1F3B';
+
+	/* gray */
+
+	static gray = '#E4E9F0';
+
+	static gray100 = '#EDEEF3';
+
+	static gray150 = '#D9D9D9';
+
+	static gray200 = '#E4E7EE';
+
+	static gray300 = '#D6DAE4';
+
+	static gray400 = '#C0C4CF';
+
+	static gray500 = '#ADB3C1';
+
+	static gray600 = '#9DA4B5';
+
+	static gray700 = '#8D95A8';
+
+	static gray800 = '#6B748B';
+
+	static gray900 = '#383C46';
+
+	static gray950 = '#2E3139';
+
+	/* blue */
+
+	static blue100 = '#E9F1FB';
+
+	static blue200 = '#D4E3F4';
+
+	static blue300 = '#B4CEED';
+
+	static blue400 = '#7DA8DB';
+
+	static blue500 = '#4786D0';
+
+	static blue600 = '#1B5CA8';
+
+	static blue700 = '#003777';
+
+	static blue800 = '#03162C';
+
+	/* line */
+
+	static line = '#F3F3F3';
+
+	/* table */
+
+	static table1 = '#E4E9F0';
+
+	static table2 = '#0058A5';
+
+	/* select */
+	static select = '#767676';
+}
