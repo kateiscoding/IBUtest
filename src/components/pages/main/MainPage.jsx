87d566a8514c12103a457b0a6d6119f8ac5bbc71
kateiscoding <kiaksh717@gmail.com>
2023-12-08 12:26:37 -0800

    style: changed main css

--- src/components/pages/main/MainPage.jsx
+++ src/components/pages/main/MainPage.jsx
@@ -114,7 +114,7 @@ const Counter = styled.div`
 `;
 
 const EarthContainer = styled.div`
-	width: 150%;
+	${'' /* width: 150%; */}
 	${'' /* display: flex; */}
 	overflow: hidden;
 	${
@@ -123,7 +123,7 @@ const EarthContainer = styled.div`
 	flex-direction: column;
 	align-items: center; */
 	}
-
+	width: 200%;
 	display: flex;
 	justify-content: center;
 	align-items: flex-start;
@@ -140,16 +140,13 @@ const EarthImageComponent = styled.img`
 	position: relative; */
 	}
  /* 이미지의 최대 너비 */
-	max-width: 100%; /* 부모 요소의 너비에 맞게 조정 */
+	max-width: 120%; /* 부모 요소의 너비에 맞게 조정 */
 	display: block; /* 이미지 간격과 레이아웃을 조정하기 위해 사용 */
 	overflow: hidden;
 
 	${'' /* background-image: url(${EarthImage}); */}
 	background-size: cover;
-	${'' /* position: relative; */}
-	${'' /* top: -60rem; */}
-	width: 900rem; /* 이미지의 최대 너비 */
-	max-width: 100%; /* 화면에 맞게 조정 */
+	width: 964rem;
 	display: block;
 	overflow: hidden;
 	/* 중앙 정렬을 위한 마진 수정 */
