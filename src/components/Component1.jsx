fa2e19c4054c9795a001c4fe54a2bceb847e5a27
kateiscoding <kiaksh717@gmail.com>
2023-12-28 10:22:43 -0800

    feat: updated daily test

--- src/components/Component1.jsx
+++ src/components/Component1.jsx
@@ -24,7 +24,7 @@ function FirstComponent() {
 					</div>
 					{/* <p>110,000,378</p> */}
 				</div>
-				<CountUp start={100000000} end={111521500} duration={3} />
+				<CountUp start={100000000} end={111894900} duration={3} />
 			</Counter>
 			<FlagContainer />
 		</Component1>
