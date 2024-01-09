a6783c57e1986e6ef5bb8f19004acf3594ac0c4a
kateiscoding <kiaksh717@gmail.com>
2024-01-08 16:46:35 -0800

    feat: adding industry card component

---
+++ src/components/IndustryLargeBottomRight.jsx
@@ -0,0 +1,111 @@
+import React, { useRef, useState, useEffect } from 'react';
+import * as THREE from 'three';
+import styled, { keyframes } from 'styled-components';
+import { Canvas } from '@react-three/fiber';
+import { Image } from '@react-three/drei';
+import industryImage from '../assets/img-industry.png';
+
+function IndustryLargeBottomRight({ title, caption, image }) {
+	return (
+		<IndustryCardLargeBottomRight>
+			<div>
+				<div>
+					<div>
+						<p>{title}</p>
+						<p>{caption}</p>
+					</div>
+				</div>
+			</div>
+			<div>
+				{/* 사진 */}
+				<img src={image} alt='industry-sample' />
+			</div>
+		</IndustryCardLargeBottomRight>
+	);
+}
+
+const IndustryCardLargeBottomRight = styled.div`
+	width: 63.8rem;
+	height: 63.8rem;
+
+	${'' /* 일단 해봄 */}
+	display: flex;
+	flex-direction: column;
+	align-items: flex-end;
+	justify-content: flex-end;
+	position: relative;
+
+	> div:nth-child(1) {
+		width: 54rem;
+		height: 54rem;
+		flex-shrink: 0;
+		top: 0;
+		left: 0;
+		border-radius: 54rem;
+		display: flex;
+		border: 4px dashed var(--BandCI_dark, #6c1624);
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		position: absolute;
+		z-index: 4;
+
+		> div {
+			border-radius: 18.4rem;
+			border: 2px solid var(--BandCI_dark, #6c1624);
+			background: var(--bwb-07, rgba(63, 63, 63, 0.7));
+			box-shadow: 0px 4px 30px 0px rgba(0, 0, 0, 0.3);
+			backdrop-filter: blur(40px);
+			display: flex;
+			width: 36.8rem;
+			height: 36.8rem;
+			padding: 0.8rem 5.6rem;
+			flex-direction: column;
+			justify-content: center;
+			align-items: flex-start;
+			gap: 3.2rem;
+			flex-shrink: 0;
+			align-items: center;
+
+			> div {
+				display: flex;
+				gap: 3.2rem;
+				flex-direction: column;
+
+				> p:nth-child(1) {
+					color: white;
+					text-align: center;
+					font-size: 4rem;
+					font-style: normal;
+					font-weight: 600;
+					line-height: 100%; /* 4rem */
+					align-self: stretch;
+				}
+
+				> p:nth-child(2) {
+					color: var(--BW-White, #fff);
+					text-align: center;
+
+					font-size: 2.4rem;
+					font-style: normal;
+					font-weight: 400;
+					line-height: 120%; /* 2.88rem */
+					letter-spacing: -0.06rem;
+				}
+			}
+		}
+	}
+
+	> div:nth-child(2) {
+		width: 47.2rem;
+		height: 47.2rem;
+		right: -8rem;
+		bottom: 0;
+		flex-shrink: 0;
+		display: flex;
+		z-index: 3;
+		position: absolute;
+	}
+`;
+
+export default IndustryLargeBottomRight;
